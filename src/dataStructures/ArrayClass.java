2e00550bdc0b522c94426484886e09cf2c7c29fa
Unish Rajkarnikar <ubidy947@gmail.com>
2024-02-13 18:55:54 -0600

    linear searching

---
+++ src/dataStructures/ArrayClass.java
@@ -0,0 +1,8 @@
+package dataStructures;
+
+public class ArrayClass {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dataStructures/ArrayDS.java
@@ -0,0 +1,15 @@
+package dataStructures;
+
+import java.util.Arrays;
+
+public class ArrayDS {
+
+	public static void main(String[] args) {
+		String[] arr = new String[5];
+		int[] ros;
+		
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
